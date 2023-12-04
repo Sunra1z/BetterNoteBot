627ea6db4ca274b84fb88a41b70e79807e82913a
elik <tokaev.04@gmail.com>
2023-12-04 21:32:55 +0600

    Reverted bcs of the bug >:(

--- app/handlers.py
+++ app/handlers.py
@@ -52,23 +52,16 @@ async def end_note(message: Message, state: FSMContext):
 @router.message(WaitingForNoteText.gpt_thinker)
 async def gpt_think(message: Message, state: FSMContext):
     reminder_time_str = message.text
+    note_text = await state.get_data()
     try:
         reminder_time = datetime.datetime.strptime(reminder_time_str, "%d/%m/%Y %H:%M") #TODO: Add timezone adaptation for reminder system (shit works only in KZ lmao)
     except ValueError:
         await message.answer("Неверный формат даты! Введите дату в формате ДД/ММ/ГГГГ ЧЧ:ММ")
         return
     await message.answer("Будет сделано! Напомню вам в" + ' ' + reminder_time_str)
-    await state.update_data(reminder_time=reminder_time) # Passing reminder time
-    await state.set_state(WaitingForNoteText.type_reminder)
-
-
-@router.message(WaitingForNoteText.type_reminder) #TODO: Fix a bug: can't use commands like today notes or show notes when delay is set
-async def delay_counter(message: Message, state: FSMContext):
-    note_text = await state.get_data()
-    reminder_time = await state.get_data()
     user_id = message.from_user.id
-    await store_note(user_id, note_text['note_text'], reminder_time['reminder_time'])
-    delay = (reminder_time['reminder_time'] - datetime.datetime.now()).total_seconds()
+    await store_note(user_id, note_text['note_text'], reminder_time)
+    delay = (reminder_time - datetime.datetime.now()).total_seconds()
     print(note_text['note_text'])
     if delay > 0:
         await asyncio.sleep(delay)
@@ -76,6 +69,7 @@ async def delay_counter(message: Message, state: FSMContext):
     else:
         await message.answer("Время напоминания уже прошло! Введите другое время напоминания!")
         await state.set_state(WaitingForNoteText.gpt_thinker)
+
 @router.message(F.text.lower() == 'посмотреть заметки') # Handler for getting all notes sorted by user ID
 async def notesShow(message: Message):
     notes = await get_notes(message.from_user.id)
@@ -84,16 +78,6 @@ async def notesShow(message: Message):
             await message.answer(note.text)
     else:
         await message.answer("У вас нет заметок!")
-
-@router.message(F.text.lower() == 'сегодняшние заметки') # Handler for getting all notes sorted by time
-async def today_notes(message: Message, state: FSMContext):
-    notes = await get_notes(message.from_user.id)
-    if notes:
-        for note in notes:
-            if note.reminder_time.date() == datetime.datetime.now().date():
-                await message.answer(note.text)
-    else:
-        await message.answer("У вас нет заметок на сегодня!")
 
 # @router.message(F.text.lower() == 'отмена') # TODO: Cancel any operation with bot and return to the start
 # async def cancel(message: Message, state: FSMContext):
--- app/keyboards.py
+++ app/keyboards.py
@@ -4,7 +4,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 main_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Создать заметку')],
     [KeyboardButton(text='Посмотреть заметки')],
-    [KeyboardButton(text='Сегодняшние заметки')],
+    [KeyboardButton(text='Бесполезная кнопка')]
 ],
 resize_keyboard=True,
 one_time_keyboard=True,
