b65a4c55c4fb7f3d5f8c70ed3431b037da61c6a9
elik <tokaev.04@gmail.com>
2023-12-04 13:17:42 +0600

    Today notes update

--- app/keyboards.py
+++ app/keyboards.py
@@ -4,7 +4,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 main_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Создать заметку')],
     [KeyboardButton(text='Посмотреть заметки')],
-    [KeyboardButton(text='Бесполезная кнопка')]
+    [KeyboardButton(text='Сегодняшние заметки')],
 ],
 resize_keyboard=True,
 one_time_keyboard=True,
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = '6613135673:AAGFo5uBwfHlnEUWSkI78pk211moq4s_ZQc'
 SQLALCHEMY_URL = 'sqlite+aiosqlite:///db.sqlite3'
-OpenAiKey = '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'
+OpenAiKey = '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'
