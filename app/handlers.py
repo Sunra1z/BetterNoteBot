f7fc3fabb6eb0449a08b038d4a21fb0e6b86d829
elik <tokaev.04@gmail.com>
2023-12-06 22:00:55 +0600

    Server deployment

--- app/handlers.py
+++ app/handlers.py
@@ -3,10 +3,7 @@ import datetime
 import asyncio
 
 
-import openai
-
 from app.keyboards import main_kb
-from config import OpenAiKey
 from aiogram import Router, F
 from app.database.requests import get_notes
 
@@ -16,18 +13,17 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 from app.database.models import store_note
 
-openai.api_key = OpenAiKey
-router = Router()
+router = Router() # Router for message handlers, used to create main functions in different files, and not in one big file
 
 
 class WaitingForNoteText(StatesGroup): # States for creating notes
     typing_note = State()
     type_reminder = State()
-    gpt_thinker = State()
+    gpt_thinker = State() # nvm I can't name things properly
     note_storage = State()
 
 
-@router.message(CommandStart())
+@router.message(CommandStart()) # Handler for /start command
 async def cmd_start(message: Message):
     await message.answer('Добро пожаловать!')
     await message.answer('Я бот который поможет тебе делать заметки!', reply_markup=main_kb)
@@ -45,29 +41,29 @@ async def end_note(message: Message, state: FSMContext):
     await state.set_state(WaitingForNoteText.gpt_thinker) # Setting state as WAITING for reminder
     await state.update_data(note_text=note_text) # Updating data for reminder
 
-@router.message(WaitingForNoteText.gpt_thinker)
+@router.message(WaitingForNoteText.gpt_thinker) # Handler for remind time STEP 3 FOR STATES
 async def gpt_think(message: Message, state: FSMContext):
     reminder_time_str = message.text
     note_text = await state.get_data()
     try:
         reminder_time = datetime.datetime.strptime(reminder_time_str, "%d/%m/%Y %H:%M") #TODO: Add timezone adaptation for reminder system (shit works only in KZ lmao)
-    except ValueError:
+    except ValueError: # Error handler for invalid date format
         await message.answer("Неверный формат даты! Введите дату в формате ДД/ММ/ГГГГ ЧЧ:ММ")
         return
     delay = (reminder_time - datetime.datetime.now()).total_seconds()
-    if delay < 0:
+    if delay < 0: # Error handler for a past date input
         await message.answer("Время напоминания уже прошло! Введите другое!")
         await state.set_state(WaitingForNoteText.gpt_thinker)
     else:
         await message.answer("Будет сделано! Напомню вам в" + ' ' + reminder_time_str)
         user_id = message.from_user.id
-        await store_note(user_id, note_text['note_text'], reminder_time)
-        await state.clear()
-        rmndr = asyncio.create_task(delay_counter(delay))
+        await store_note(user_id, note_text['note_text'], reminder_time) # Saving correct note data to a DB
+        await state.clear() # Clearing state to let user use other commands
+        rmndr = asyncio.create_task(delay_counter(delay)) # Creating asynchronous task for reminder system (Helps with multitasking!)
         await rmndr
         await message.answer("Напоминаю!" + ' ' + note_text['note_text'])
 @router.message(F.text.lower() == 'посмотреть заметки') # Handler for getting all notes sorted by user ID
-async def notesShow(message: Message):
+async def notesShow(message: Message): # Func to retrieve and show all notes from DB by user ID (used only for debugging lol)
     notes = await get_notes(message.from_user.id)
     if notes:
         for note in notes:
@@ -75,17 +71,17 @@ async def notesShow(message: Message):
     else:
         await message.answer("У вас нет заметок!")
 
-@router.message(F.text.lower() == 'заметки на сегодня')
+@router.message(F.text.lower() == 'заметки на сегодня') # Func to retrieve and show all today notes from DB by user ID
 async def todayNotesShow(message: Message):
     notes = await get_notes(message.from_user.id)
     if notes:
         for note in notes:
-            if note.reminder_time.date() == datetime.datetime.now().date():
-                await message.answer(note.text + ' ' + note.reminder_time.strftime('%H:%M'))
+            if note.reminder_time.date() == datetime.datetime.now().date(): # Sorting notes only by date, regardless of time
+                await message.answer(note.text + ' ' + note.reminder_time.strftime('%H:%M')) # Showing only time and text of a note
     else:
         await message.answer("На сегодня нет заметок!")
 
 
-async def delay_counter(delay):
+async def delay_counter(delay): # That is a reminder system, it works by delaying a message for a certain amount of time (Works asynchronously tho!)
     if delay > 0:
         await asyncio.sleep(delay)
--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 TOKEN = '6613135673:AAGFo5uBwfHlnEUWSkI78pk211moq4s_ZQc'
 SQLALCHEMY_URL = 'sqlite+aiosqlite:///db.sqlite3'
-OpenAiKey = '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'
