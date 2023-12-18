c5c353f2b4bad236be7ac85a80de58ace6acb42c
Kassymzhomart Tokayev <70940419+Sunra1z@users.noreply.github.com>
2023-12-19 01:05:05 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = '6613135673:AAGFo5uBwfHlnEUWSkI78pk211moq4s_ZQc' #TODO: If commiting to main branch, change to the correct one
+TOKEN = 'token here' #TODO: If commiting to main branch, change to the correct one
 SQLALCHEMY_URL = 'sqlite+aiosqlite:///db.sqlite3'
